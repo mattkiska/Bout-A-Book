f76b4564b15627075a4942b3d733d2c68e94b52c
Matt <mkiska1@gmail.com>
2022-10-19 19:12:51 -0400

    Added social links to bottom of page and made them clickable. Also updated JS added more functions.

--- README.md
+++ README.md
@@ -18,3 +18,5 @@ It also has an the option to search by author, this will proivde:
 ### Our goal is to put everything you want to know about your book in one easy place for you and anyone else to find.
 
 Bout-A-Book utilizes Openlibrary.org for all of the API usage.
+
+Also, if you'd like to contact me or find more of my work, click the links at the bottom!
--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 console.log('working')
 
-let button = document.querySelector('#searchButton')
+const button = document.querySelector('#searchButton')
 
 
 
 async function getData(event){
 
-    event.preventDefault()
-
 
     let bookSearch = document.querySelector("#searchBox").value.toLowerCase()
 
-fetch(`https://openlibrary.org/search.json?q=${bookSearch}`)
+fetch(`http://openlibrary.org/search.json?q=${bookSearch}`)
     .then(res => {
         return res.json()
     })
     .then(res => {
-        console.log('success', res.name)
-        let bookAuthor = document.querySelector('#author')
-        bookAuthor.innerText = res.editions
+        let book = document.querySelector('#book')
+        book.style.opacity = 100
+
+        for(var i = 0; i < 1; i++){
+            document.getElementById("information").innerHTML+="<h2>"+res.docs[i].title+"</h2>"+"<br><img src='http://covers.openlibrary.org/b/isbn/"+res.docs[i].isbn[0]+"-M.jpg'><br>";
+        }
+        for(var i = 0; i < 1; i++){
+            document.getElementById("author").innerHTML+="<h3>" + 'Author: ' +res.docs[i].author_name[0]
+        }
+        for(var i = 0; i < 1; i++){
+            document.getElementById("moreinfo").innerHTML+="<h3>" + 'Published by: ' +res.docs[i].publisher[0]
+        }
+        for(var i = 0; i < 1; i++){
+            document.getElementById("addlinfo").innerHTML+="<h3>" + 'Genre: ' +res.docs[i].subject_facet[0]
+        }
+        for(var i = 0; i < 1; i++){
+            document.getElementById("firstYear").innerHTML+="<h3>" + 'Published in ' +res.docs[i].first_publish_year
+        }
+        for(var i = 0; i < 1; i++){
+            document.getElementById("pageCount").innerHTML+="<h3>"+res.docs[i].number_of_pages_median + ' pages'
+        }
     })
 
 
-
+ 
 }
 
 button.addEventListener('click', getData)
