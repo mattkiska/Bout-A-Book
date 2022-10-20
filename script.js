53996d28e29c98837dc42cf3130280572bc3a1c0
Matt <mkiska1@gmail.com>
2022-10-20 08:54:15 -0400

    Lightened up code in js

--- script.js
+++ script.js
@@ -8,33 +8,28 @@ async function getData(event){
 
 
     let bookSearch = document.querySelector("#searchBox").value.toLowerCase()
-
+    let book = document.querySelector('#book')
+   
 fetch(`http://openlibrary.org/search.json?q=${bookSearch}`)
     .then(res => {
         return res.json()
     })
     .then(res => {
-        let book = document.querySelector('#book')
+        book = document.querySelector('#book')
         book.style.opacity = 100
 
-        for(var i = 0; i < 1; i++){
-            document.getElementById("information").innerHTML+="<h2>"+res.docs[i].title+"</h2>"+"<br><img src='http://covers.openlibrary.org/b/isbn/"+res.docs[i].isbn[0]+"-M.jpg'><br>";
-        }
-        for(var i = 0; i < 1; i++){
-            document.getElementById("author").innerHTML+="<h3>" + 'Author: ' +res.docs[i].author_name[0]
-        }
-        for(var i = 0; i < 1; i++){
-            document.getElementById("moreinfo").innerHTML+="<h3>" + 'Published by: ' +res.docs[i].publisher[0]
-        }
-        for(var i = 0; i < 1; i++){
-            document.getElementById("addlinfo").innerHTML+="<h3>" + 'Genre: ' +res.docs[i].subject_facet[0]
-        }
-        for(var i = 0; i < 1; i++){
-            document.getElementById("firstYear").innerHTML+="<h3>" + 'Published in ' +res.docs[i].first_publish_year
-        }
-        for(var i = 0; i < 1; i++){
-            document.getElementById("pageCount").innerHTML+="<h3>"+res.docs[i].number_of_pages_median + ' pages'
-        }
+            document.getElementById("information").innerHTML="<h2>"+res.docs[0].title+"</h2>"+"<br><img src='http://covers.openlibrary.org/b/isbn/"+res.docs[0].isbn[0]+"-M.jpg'><br>";
+        
+            document.getElementById("author").innerHTML="<h3>" + 'Author: ' +res.docs[0].author_name[0]
+        
+            document.getElementById("moreinfo").innerHTML="<h3>" + 'Published by: ' +res.docs[0].publisher[0]
+        
+            document.getElementById("addlinfo").innerHTML="<h3>" + 'Genre: ' +res.docs[0].subject_facet[0]
+        
+            document.getElementById("firstYear").innerHTML="<h3>" + 'Published in ' +res.docs[0].first_publish_year
+        
+            document.getElementById("pageCount").innerHTML="<h3>"+res.docs[0].number_of_pages_median + ' pages'
+        
     })
 
 
